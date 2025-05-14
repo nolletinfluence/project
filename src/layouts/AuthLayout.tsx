8f0da5fdf46a205054cd11a30fc22a78fb2a26fa
nolletinfluence <borkoev65@gmail.com>
2025-05-15 00:56:24 +0600

    added fonts + decoration

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Character from '../components/Character/Character';
 import Decorations from '../components/Decorations/Decorations';
-const { SplatterOverlay, DiamondDecoration } = Decorations;
+const { SplatterOverlay, DiamondDecoration, GunDecoration } = Decorations;
 import styles from './AuthLayout.module.css';
 
 interface AuthLayoutProps {
@@ -28,12 +28,22 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ title, subtitle, children, char
       />
 
       {/* Пистолет в левом нижнем углу */}
-      <div className={styles.gunWrapper}>
-      <div className={styles.gunLeft} />
-      </div>
+        <div className={styles.gunWrapper}>
+          <div className={styles.gunLeft} />
+          
+          <div className={styles.gunDecorationWrapper}>
+            <GunDecoration className={styles.gunDecoration} />
+          </div>
+        </div>
 
       {/* Пистолет в правом верхнем углу */}
-      {/* <div className={styles.gunRight} /> */}
+      <div className={styles.gunWrapperRight}>
+        <div className={styles.gunRight} />
+      
+        <div className={styles.gunDecorationWrapperRight}>
+          <GunDecoration className={styles.gunDecoration} />
+        </div>
+      </div>
 
       {/* Контент */}
       <div className={styles.content}>
@@ -52,16 +62,20 @@ const AuthLayout: React.FC<AuthLayoutProps> = ({ title, subtitle, children, char
         <div className={styles.rightSection}>
           <div className={styles.logoWrapper}>
               <p className={styles.subtitle}>{subtitle}</p>
-              <h1 className={styles.title}>{title}</h1>
+              <h1 font-family="Chinese Rocks" className={styles.title}>{title}</h1>
               <div className="w-full max-w-md relative mt-8">
               {children}
               <DiamondDecoration className={styles.diamondDecoration} />
               <DiamondDecoration className={styles.diamondDecorationRightTop} />
               <DiamondDecoration className={styles.diamondDecorationLeftBottom} />
               <DiamondDecoration className={styles.diamondDecorationRightBottom} />
+              </div>
+              <div className={styles.fact}>
+                <span>*</span> <br />
+              <span>Интересный факт #667</span>
+              <p>Corem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan</p>
+              </div>
           </div>
-          </div>
-
         </div>
       </div>
     </div>
